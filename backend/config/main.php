4775817b3993a9bcefaef4618ad349f841a95734
serge_ <jumpaper@gmail.com>
2019-02-03 01:06:50 +0300

    авторизация в бэке и в фронте

--- backend/config/main.php
+++ backend/config/main.php
@@ -19,13 +19,14 @@ return [
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity', 'httpOnly' => true,],
+            'identityCookie' => ['name' => '_identity', 'httpOnly' => true, 'domain'=>'.yii2', 'path'=>'/'],
 
         ],
         'session' => [
 
             // this is the name of the session cookie used for login on the backend
             'name' => 'advanced',
+            'savePath'=> __DIR__ . '/../../sessions'
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
@@ -47,13 +48,13 @@ return [
             ],
         ],
         */
-/*        'view' => [
+        'view' => [
             'theme' => [
                 'pathMap' => [
                     '@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app'
                 ],
             ],
-        ],*/
+        ],
     ],
     'params' => $params,
 ];
--- frontend/config/main.php
+++ frontend/config/main.php
@@ -18,12 +18,13 @@ return [
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity', 'httpOnly' => true,],
+            'identityCookie' => ['name' => '_identity', 'httpOnly' => true, 'domain'=>'.yii2', 'path'=>'/'],
 
         ],
         'session' => [
             // this is the name of the session cookie used for login on the frontend
             'name' => 'advanced',
+            'savePath'=> __DIR__ . '/../../sessions'
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
